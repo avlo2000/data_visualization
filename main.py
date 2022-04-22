08da12b4ba631f2066447273016e58bbabd4c465
pavlo <pmykalov@gmail.com>
2022-04-22 17:31:31 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+
+if __name__ == '__main__':
+    df = pd.read_csv('data/Dataset of Diabetes .csv')
+
+    print(df.columns)
+    df = df.drop(columns=['ID'])
+    print(df.head(10))
+
+    df_PCA = df.drop(columns=['Gender', 'CLASS'])
+    np_pca = df_PCA.to_numpy()
+
+    scaler = preprocessing.MinMaxScaler()
+    x_scaled = scaler.fit_transform(np_pca)
+
+    pca = PCA(n_components=len(df_PCA.columns))
+    pca.fit(x_scaled)
+    print(pca.explained_variance_ratio_)
+    print(pca.singular_values_)
+
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('Number of components')
+    plt.ylabel('Cumulative explained variance')
+    plt.show()
+
