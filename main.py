0d95846b6bd75359ac330ffbcdf25dcd98d6ee7b
pavlo <pmykalov@gmail.com>
2022-04-22 18:03:45 +0300

    A

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
+from keras.utils import to_categorical
 
-if __name__ == '__main__':
-    df = pd.read_csv('data/Dataset of Diabetes .csv')
 
-    print(df.columns)
-    df = df.drop(columns=['ID'])
-    print(df.head(10))
-
+def do_pca(df):
     df_PCA = df.drop(columns=['Gender', 'CLASS'])
     np_pca = df_PCA.to_numpy()
 
@@ -26,4 +22,26 @@ if __name__ == '__main__':
     plt.xlabel('Number of components')
     plt.ylabel('Cumulative explained variance')
     plt.show()
+
+
+def prepare_data(df: pd.DataFrame):
+    y = pd.Categorical(df['CLASS']).codes
+
+    df = df.drop(columns=['CLASS'])
+    df['Gender'] = pd.Categorical(df['Gender']).codes
+    x = df.to_numpy()
+
+
+def main():
+    df = pd.read_csv('data/Dataset of Diabetes .csv')
+
+    print(df.columns)
+    df = df.drop(columns=['ID'])
+    print(df.head(10))
+
+    do_pca(df)
+
+
+if __name__ == '__main__':
+    main()
 
